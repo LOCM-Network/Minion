b4b252aa7f03c4c4ae375444b7259eb8c4b0bb39
Labality <56186498+Labality@users.noreply.github.com>
2021-09-29 11:15:40 +0700

    Add files via upload

--- src/main/java/me/labalityowo/minions/Minion.java
+++ src/main/java/me/labalityowo/minions/Minion.java
@@ -6,12 +6,17 @@ import cn.nukkit.entity.EntityHuman;
 import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.item.Item;
+import cn.nukkit.level.Position;
 import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.math.Vector3;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.utils.TextFormat;
 import com.nukkitx.fakeinventories.inventory.ChestFakeInventory;
 import com.nukkitx.fakeinventories.inventory.DoubleChestFakeInventory;
 import com.nukkitx.fakeinventories.inventory.FakeInventory;
+import ru.contentforge.formconstructor.form.SimpleForm;
+import ru.contentforge.formconstructor.form.element.Button;
+import ru.contentforge.formconstructor.form.handler.SimpleFormHandler;
 
 import java.util.Arrays;
 
@@ -31,12 +36,15 @@ public abstract class Minion extends EntityHuman {
         FULL_INVENTORY
     }
 
-    private String owner = null;
     private FakeInventory inventory;
 
     private MinionStatus status;
 
     private final int level = 1;
+
+    private int triggerAfterSecond = 0;
+
+    private int workingTick = 0;
 
     public Minion(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
@@ -84,27 +92,79 @@ public abstract class Minion extends EntityHuman {
         switch(level){
             case 1:
                 inventory = new ChestFakeInventory();
+                triggerAfterSecond = 3 * 100;
                 break;
             case 2:
                 inventory = new DoubleChestFakeInventory();
+                triggerAfterSecond = 2 * 100;
                 break;
         }
         super.initEntity();
     }
 
+    /* PocketMine-MP */
+
+    public void lookAt(Vector3 target){
+        double horizontal = Math.sqrt(Math.pow(target.x - this.x, 2) + Math.pow(target.z - this.z, 2));
+        double vertical = target.y - this.y;
+        setPitch(-Math.atan2(vertical, horizontal) / Math.PI * 180);
+        double xDist = target.x - this.x;
+        double zDist = target.z - this.z;
+        setYaw(Math.atan2(zDist, xDist) / Math.PI * 180 - 90);
+        if(getYaw() < 0){
+            setYaw(getYaw() + 360.0);
+        }
+    }
+
     @Override
     public boolean onUpdate(int currentTick) {
         boolean onUpdate = super.onUpdate(currentTick);
-        if(!inventory.canAddItem(Item.get(0))){
-            setStatus(MinionStatus.FULL_INVENTORY);
+        if(getStatus() == MinionStatus.FULL_INVENTORY){
             return onUpdate;
         }
-        if(!(Arrays.stream(getTargetBlocks()).anyMatch(block -> getInventory().getItemInHand().useOn(block)))){
+
+        if(!doToolCheck()){
             setStatus(MinionStatus.INEFFICIENT_TOOL);
             return onUpdate;
         }
+
         setStatus(MinionStatus.WORKING);
+
+        if(workingTick == triggerAfterSecond){
+            //TODO: Rewrite this, messy af
+            Position pos = getPosition();
+            for(int x = -3; x < 4; x++) {
+                for (int y = -3; y < 4; y++) {
+                    for (int z = -3; z < 4; z++) {
+                        Block block = getLevel().getBlock(pos.add(x, y, z));
+                        if(Arrays.stream(getTargetBlocks()).anyMatch(target -> block.getId() == target.getId())){
+                            block.onBreak(getInventory().getItemInHand());
+                            Item[] drops = block.getDrops(getInventory().getItemInHand());
+                            Arrays.stream(drops).forEach(drop -> {
+                                if(!inventory.canAddItem(drop)){
+                                    setStatus(MinionStatus.FULL_INVENTORY);
+                                    return;
+                                }
+                                inventory.addItem(drop);
+                            });
+                            break;
+                        }
+                    }
+                }
+            }
+            workingTick = 0;
+            return onUpdate;
+        }
+        workingTick++;
         return onUpdate;
+    }
+
+    public boolean doToolCheck(){
+        return Arrays.stream(getTargetBlocks()).anyMatch(block -> getInventory().getItemInHand().useOn(block));
+    }
+
+    public boolean doToolCheck(Item item){
+        return Arrays.stream(getTargetBlocks()).anyMatch(block -> item.useOn(block));
     }
 
     @Override
@@ -117,13 +177,44 @@ public abstract class Minion extends EntityHuman {
         if(!(source1.getDamager() instanceof Player)){
             return true;
         }
-        if(!(source1.getDamager().getName().equals(getOwner()))){
+        Player damager = (Player) source1.getDamager();
+        if(!(damager.getName().equals(getOwner()))){
             return true;
         }
-        System.out.println("sent inventory !!!!!");
-        Player owner = ((Player) source1.getDamager());
-        owner.addWindow(inventory);
-        System.out.println("sent inventory !!!!!");
+        sendForm(damager);
         return true;
+    }
+
+    public void sendForm(Player owner){
+        SimpleForm form = new SimpleForm(getOwner() + "'s minion");
+        form.addButton(new Button("Change minion tool.", (p, button) -> {
+            Item minionTool = getInventory().getItemInHand();
+            Item playerTool = p.getInventory().getItemInHand();
+            if(!doToolCheck(playerTool)){
+                p.sendMessage("Ko hop item");
+                return;
+            }
+            getInventory().setItemInHand(playerTool);
+            p.getInventory().setItemInHand(minionTool);
+        }));
+        if(!getInventory().getItemInHand().equals(Item.get(0))){
+            form.addButton(new Button("Remove minion tool.", (p, button) -> {
+                Item minionTool = getInventory().getItemInHand();
+                if(!p.getInventory().canAddItem(minionTool)){
+                    p.sendMessage("Tui do da day");
+                    return;
+                }
+                getInventory().setItemInHand(Item.get(0));
+                p.getInventory().addItem(minionTool);
+            }));
+        }
+        form.addButton(new Button("View minion inventory.", (p, button) -> {
+                p.addWindow(inventory);
+        }));
+        form.addButton(new Button("Despawn minion.", (p, button) -> {
+            close();
+            p.sendMessage("Đã thu minion lại thành 1 item");
+        }));
+        form.send(owner);
     }
 }
